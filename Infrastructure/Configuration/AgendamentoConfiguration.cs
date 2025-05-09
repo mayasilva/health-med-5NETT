fd8eca49ad4c682ec41040d19a1874b82385d8b4
Mayara Alves da Silva <mayara.silva28@gmail.com>
2025-05-09 14:04:28 -0300

    cria tabela agendamento

---
+++ Infrastructure/Configuration/AgendamentoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository.Configurations
+{
+    public class AgendamentoConfiguration : IEntityTypeConfiguration<Agendamento>
+    {
+        public void Configure(EntityTypeBuilder<Agendamento> builder)
+        {
+            builder.ToTable("agendamento");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Status).HasColumnType("VARCHAR(10)").IsRequired();
+            builder.Property(p => p.IdAgenda).HasColumnType("INT").IsRequired();
+            builder.Property(p => p.IdPaciente).HasColumnType("INT").IsRequired();
+
+            builder.HasOne(p => p.Agenda)
+                .WithMany(a => a.Agendamentos)
+                .HasForeignKey(p => p.IdAgenda);
+
+            builder.HasOne(p => p.Paciente)
+                .WithMany(a => a.Agendamentos)
+                .HasForeignKey(a => a.IdPaciente)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20250509170114_Agendamento.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agendamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "agendamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "VARCHAR(10)", nullable: false),
+                    IdAgenda = table.Column<int>(type: "INT", nullable: false),
+                    IdPaciente = table.Column<int>(type: "INT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_agendamento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_agendamento_agenda_IdAgenda",
+                        column: x => x.IdAgenda,
+                        principalTable: "agenda",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_agendamento_paciente_IdPaciente",
+                        column: x => x.IdPaciente,
+                        principalTable: "paciente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_agendamento_IdAgenda",
+                table: "agendamento",
+                column: "IdAgenda");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_agendamento_IdPaciente",
+                table: "agendamento",
+                column: "IdPaciente");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "agendamento");
+        }
+    }
+}
