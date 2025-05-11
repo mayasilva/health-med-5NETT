a4b89d9aaf1f4b4da08c8038727c940eb80545ec
Mayara Alves da Silva <mayara.silva28@gmail.com>
2025-05-11 12:29:19 -0300

    exclui endpoint nao usado

--- Api/Controllers/MedicoController.cs
+++ Api/Controllers/MedicoController.cs
@@ -98,51 +98,6 @@ namespace Api.Controllers
             }
         }
 
-
-        /// <summary>
-        /// Necessita de autenticação via token para cadastrar um novo contato
-        /// </summary>
-        /// <remarks>
-        /// Exemplo de requisição:
-        /// 
-        /// {
-        ///     "Nome": "Nome do Contato",
-        ///     "DDD": "DDD (Região) do telefone do contato",
-        ///     "Telefone": "Telefone do Contato",
-        ///     "Email": "Email do Contato",
-        /// }
-        /// 
-        /// Observação: Não é necessario informar o Id
-        /// </remarks>
-        /// <param name="input">Objeto do ContatoInput</param>
-        /// <returns>Retorna Contato cadastrado</returns>
-        /// <response code="200">Sucesso na execução da inclusão do contato na fila-cadastro</response>
-        /// <response code="500">Não foi possivel incluir um novo contato</response>
-        /// <response code="401">Token inválido</response>
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] ContatoInput input)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    var endpoint = await _bus.GetSendEndpoint(new Uri("queue:FilaCadasto"));
-                    await endpoint.Send(input);
-                    return Ok();
-                }
-                else
-                {
-                    return BadRequest(ModelState);
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e);
-            }
-
-        }
-
         /// <summary>
         /// Necessita de autenticação via token para excluir uma agenda
         /// </summary>
